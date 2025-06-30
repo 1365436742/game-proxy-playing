6d01a1c90afdc761f03112f78883d1dde34c869f
1365436742 <51362520+1365436742@users.noreply.github.com>
2025-06-30 15:18:16 +0800

    Update userAPI.ts

--- web-code/mock/userAPI.ts
+++ web-code/mock/userAPI.ts
@@ -1,6 +1,17 @@
 const users = [
-  { id: 0, name: 'Umi', nickName: 'U', gender: 'MALE' },
-  { id: 1, name: 'Fish', nickName: 'B', gender: 'FEMALE' },
+  {
+    id: 0,
+    name: '张卓',
+    gender: 0,
+    workStatus: 0,
+    phone: '15214586883',
+    cardId: '230702199810220318',
+    workerTime: '18:00:00-20:00:00',
+    monthNumber: 50,
+    monthWorkTime: 8,
+    monthMoney: 5000,
+  },
+  { id: 1, name: 'Fish', gender: 'FEMALE' },
 ];
 
 export default {
