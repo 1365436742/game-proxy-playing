d915da98bb37b4b20a6a8e8bffc0e8ee53095e7a
1365436742 <51362520+1365436742@users.noreply.github.com>
2025-07-01 11:14:03 +0800

    Create index.tsx

---
+++ web-code/src/pages/OrderList/index.tsx
@@ -0,0 +1,83 @@
+import {
+  ActionType,
+  PageContainer,
+  ProDescriptionsItemProps,
+  ProTable,
+} from '@ant-design/pro-components';
+import { Divider } from 'antd';
+import React, { useRef } from 'react';
+
+const TableList: React.FC<unknown> = () => {
+  const actionRef = useRef<ActionType>();
+  const columns: ProDescriptionsItemProps[] = [
+    {
+      title: '订单id',
+      dataIndex: 'id',
+    },
+    {
+      title: '创建时间',
+      dataIndex: 'createTime',
+    },
+    {
+      title: '金额',
+      dataIndex: 'money',
+    },
+    {
+      title: '用户昵称',
+      dataIndex: 'nickName',
+    },
+    {
+      title: '用户id',
+      dataIndex: 'openid',
+    },
+    {
+      title: '接单人',
+      dataIndex: 'name',
+    },
+    {
+      title: '接单人手机号',
+      dataIndex: 'phone',
+    },
+    {
+      title: '操作',
+      dataIndex: 'option',
+      valueType: 'option',
+      render: (_, record) => (
+        <>
+          <a>投诉原因</a>
+          <Divider type="vertical" />
+          <a>订单信息</a>
+          <Divider type="vertical" />
+          <a>反馈</a>
+        </>
+      ),
+    },
+  ];
+
+  return (
+    <PageContainer>
+      <ProTable
+        actionRef={actionRef}
+        rowKey="id"
+        request={async (params, sorter, filter) => {
+          return {
+            data: [
+              {
+                openid: 'xxxxx',
+                nickName: '热情仔',
+                phone: '15214586883',
+                status: 0,
+                orderId: 'xxx4557ddd',
+              },
+            ],
+            success: true,
+          };
+        }}
+        // @ts-ignore
+        columns={columns}
+      />
+    </PageContainer>
+  );
+};
+
+export default TableList;
